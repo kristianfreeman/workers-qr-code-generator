670bb45f6bb4a4049bc0294f8e86f25f238617ff
Kristian Freeman <kristian@kristianfreeman.com>
2024-09-30 10:30:02 -0500

    Update code for 2024

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const qr = require('qr-image')
+import QRCode from 'qrcode-svg'
 
 const generate = async request => {
   const { text } = await request.json()
-  const headers = { 'Content-Type': 'image/png' }
-  const qr_png = qr.imageSync(text || 'https://workers.dev')
-  return new Response(qr_png, { headers })
+  const qr = new QRCode({ content: text || 'https://workers.dev' })
+  return new Response(qr.svg(), { headers: { 'Content-Type': 'image/svg+xml' } })
 }
 
 const landing = `
@@ -13,27 +12,31 @@ const landing = `
 <input type="text" id="text" value="https://workers.dev"></input>
 <button onclick='generate()'>Generate QR Code</button>
 <p>Check the "Network" tab in your browser's developer tools to see the generated QR code.</p>
+<div id="qr-code"></div>
 <script>
-  function generate() {
-    fetch(window.location.pathname, {
+  const generate = async () => {
+    const resp = await fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text: document.querySelector("#text").value })
     })
+
+    const qr_code = document.querySelector("#qr-code");
+    const svg = await resp.text();
+    const dataUrl = "data:image/svg+xml;base64," + btoa(svg);
+    qr_code.innerHTML = "<img src='" + dataUrl + "' />";
   }
 </script>
 `
 
-async function handleRequest(request) {
-  let response
-  if (request.method === 'POST') {
-    response = await generate(request)
-  } else {
-    response = new Response(landing, { headers: { 'Content-Type': 'text/html' } })
-  }
-  return response
+export default {
+  fetch: async (request, env) => {
+    let response
+    if (request.method === 'POST') {
+      response = await generate(request)
+    } else {
+      response = new Response(landing, { headers: { 'Content-Type': 'text/html' } })
+    }
+    return response
+    }
 }
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
